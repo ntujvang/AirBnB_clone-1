b48cf0eb1d4511df0fb5d009b84f06b969dd9fab
ntujvang <96@holbertonschool.com>
2017-03-30 01:27:07 +0000

    modified for webbnb

--- models/state.py
+++ models/state.py
@@ -4,7 +4,7 @@ from os import getenv
 from sqlalchemy import Column, String, Table
 from sqlalchemy.orm import relationship, backref
 from models.base_model import BaseModel, Base
-
+import models
 
 class State(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
@@ -17,3 +17,13 @@ class State(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         super(State, self).__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            city = []
+            states_city = models.storage.all("City")
+            for c_id in states_city:
+                if states_city[c_id].state_id == self.id:
+                    city.append(states_city[c_id])
+            return city
