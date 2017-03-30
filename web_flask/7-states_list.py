3268eeb3022383869fe1d36e27734fc1ae7a2856
ntujvang <96@holbertonschool.com>
2017-03-30 17:49:34 +0000

    comments

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+python script to create a flask page that contains x amount of states
+"""
 from models import storage
 from flask import Flask, render_template
 app = Flask(__name__)
@@ -6,12 +9,18 @@ app = Flask(__name__)
 
 @app.route('/states_list')
 def states_list():
+    """
+    method to create a webpage that list states
+    """
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown(self):
+    """
+    method to tear down
+    """
     storage.close()
 
 
